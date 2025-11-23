1a2ef2eb798488d486e76ec5dc84f063a5003d5c
anna_borysova <anna@pavlyuk.org>
2025-11-23 22:25:12 +0200

    add 11 hw js

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+const formElem = document.querySelector('.form');
+formElem.addEventListener('submit', (event) => {
+ event.preventDefault();
+const query = event.target.elements['search-text'].value.trim();
+  if (!query) {iziToast.warning({
+      message: 'Please enter a search query',
+      position: 'topRight', });
+return;}
+
+  showLoader();
+  clearGallery();
+
+  getImagesByQuery(query)
+    .then(images => {
+      createGallery(images);
+      if (images.length === 0) {
+        iziToast.error({
+          message: 'No images found. Try another query.',
+          position: 'topRight',
+        });
+      }
+    })
+    .finally(() => {
+      hideLoader();
+      event.target.reset();
+    });
+    
+  });
+// .catch(err => {
+//       iziToast.error({
+//         message: 'Request failed',
+//         position: 'topRight',
+//       });
+//       throw err;
+//     });
+
+
+
