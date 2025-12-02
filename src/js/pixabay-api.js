f0073dc469d44b229acf4750508427121ebbde8b
anna_borysova <anna@pavlyuk.org>
2025-12-02 16:08:51 +0200

    add api key

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_KEY =  import.meta.env.VITE_PIXABAY_KEY;
+const API_KEY =  '53335895-4d104a908de65f21952c0d536';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export function getImagesByQuery(query) {
   return axios
     .get(BASE_URL, {
       params: {
-        key: 53335895-4d104a908de65f21952c0d536,
+        key: API_KEY,
         q: query,
         image_type: 'photo',
         orientation: 'horizontal',
